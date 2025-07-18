5d140cb94dd34514a5478f2dc78206750b1ea014
Shivam Kewat <shivamkewat0209@gmail.com>
2025-07-18 18:46:19 +0530

    minor change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/NETFLIX_CLONE",
+  base: '/', // 👈 this is the correct base for Vercel
 })
