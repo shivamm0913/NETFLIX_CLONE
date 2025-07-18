76e633c254797ed88dfd190caebb719bf4552fd3
Shivam Kewat <shivamkewat0209@gmail.com>
2025-07-18 19:13:53 +0530

    path change

--- src/pages/Home/Auth.jsx
+++ src/pages/Home/Auth.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase';
+import { auth } from '../../firebase';
 
 const AuthContext = createContext();
 
